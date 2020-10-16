0c431deab16ef9c1ce64489ac729b6718b42ac86
Dan Miller <danjm.com@gmail.com>
2020-10-16 06:33:42 -0230

    Adds swaps-gas-customization-modal and utilize in swaps

--- ui/app/pages/swaps/view-quote/view-quote.js
+++ ui/app/pages/swaps/view-quote/view-quote.js
@@ -378,9 +378,8 @@ export default function ViewQuote () {
   }
 
   const onFeeCardMaxRowClick = () => dispatch(showModal({
-    name: 'CUSTOMIZE_GAS',
-    txData: { txParams: { ...tradeTxParams, gas: maxGasLimit } },
-    isSwap: true,
+    name: 'CUSTOMIZE_METASWAP_GAS',
+    txParams: tradeTxParams,
     customGasLimitMessage: (
       approveGas
         ? t('extraApprovalGas', [hexToDecimal(approveGas)])
@@ -395,10 +394,8 @@ export default function ViewQuote () {
         }
         : null
     ),
-    useFastestButtons: true,
-    minimumGasLimit: Number(hexToDecimal(nonCustomMaxGasLimit)),
-    noFetchOnMount: true,
-    hideAdvancedTimeEstimates: true,
+    initialGasPrice: gasPrice,
+    initialGasLimit: maxGasLimit,
   }))
 
   const tokenApprovalTextComponent = (
